b3d9519feee4a959b506db842aa577f3035067ca
Kevin Koltraka <kevikoltraka157@gmail.com>
2025-05-21 15:18:13 +0200

    changes

--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -1,210 +1,244 @@
 export const portfolios = [
-   {
-      'id': 1,
-      'image': '/portfolio/Go Health/Go Health Brand Identity-01.png'
-   },
-   {
-      'id': 2,
-      'image': '/portfolio/Go Health/Go Health Brand Identity-02.png'
-   },
-
-   {
-      'id': 4,
-      'image': '/portfolio/Go Health/Go Health Brand Identity-03.png'
-   },
-   {
-      'id': 5,
-      'image': '/portfolio/Go Health/Go Health Brand Identity-04.png'
-   },
-   {
-      'id': 6,
-      'image': '/portfolio/Go Health/Go Health Brand Identity-05.png'
-   },
-
-   {
-      'id': 7,
-      'image': '/portfolio/Go Health/Go Health Brand Identity-06.png'
-   },
-   {
-      'id': 8,
-      'image': '/portfolio/Go Health/Go Health Brand Identity-07.png'
-   },
-   {
-      'id': 10,
-      'image': '/portfolio/Aredo Stones/Aredo Stones-01.jpg'
-   },
-   {
-      'id': 11,
-      'image': '/portfolio/Aredo Stones/Aredo Stones-02.jpg'
-   },
-   {
-      'id': 12,
-      'image': '/portfolio/Aredo Stones/Aredo Stones-03.jpg'
-   },
-   {
-      'id': 13,
-      'image': '/portfolio/Aredo Stones/Aredo Stones-04.jpg'
-   },
-   {
-      'id': 14,
-      'image': '/portfolio/Aredo Stones/Aredo Stones-05.jpg'
-   },
-   {
-      'id': 15,
-      'image': '/portfolio/Aredo Stones/Aredo Stones-06.jpg'
-   },
-
-   {
-      'id': 16,
-      'image': '/portfolio/autorental.png'
-   },
-   {
-      'id': 17,
-      'image': '/portfolio/autorental1.png'
-   },
-   {
-      'id': 18,
-      'image': '/portfolio/autorental2.png'
-   },
-   {
-      'id': 19,
-      'image': '/portfolio/autorental3.png'
-   },
-   {
-      'id': 23,
-      'image': '../img/dum12.jpeg'
-   },
-   {
-      'id': 20,
-      'image': '../img/dum24.jpg'
-   },
-
-   {
-      'id': 22,
-      'image': '../img/dum11.jpg'
-   },
-
-   {
-      'id': 9,
-      'image': '../img/dum10.jpg'
-   },
-]
+  {
+    id: 1,
+    image: "/portfolio/Go Health/Go Health Brand Identity-01.png",
+  },
+  {
+    id: 2,
+    image: "/portfolio/Go Health/Go Health Brand Identity-02.png",
+  },
+  {
+    id: 4,
+    image: "/portfolio/Go Health/Go Health Brand Identity-03.png",
+  },
+  {
+    id: 5,
+    image: "/portfolio/Go Health/Go Health Brand Identity-04.png",
+  },
+  {
+    id: 6,
+    image: "/portfolio/Go Health/Go Health Brand Identity-05.png",
+  },
+  {
+    id: 7,
+    image: "/portfolio/Go Health/Go Health Brand Identity-06.png",
+  },
+  {
+    id: 8,
+    image: "/portfolio/Go Health/Go Health Brand Identity-07.png",
+  },
+  {
+    id: 10,
+    image: "/portfolio/Aredo Stones/Aredo Stones-01.jpg",
+  },
+  {
+    id: 11,
+    image: "/portfolio/Aredo Stones/Aredo Stones-02.jpg",
+  },
+  {
+    id: 12,
+    image: "/portfolio/Aredo Stones/Aredo Stones-03.jpg",
+  },
+  {
+    id: 13,
+    image: "/portfolio/Aredo Stones/Aredo Stones-04.jpg",
+  },
+  {
+    id: 14,
+    image: "/portfolio/Aredo Stones/Aredo Stones-05.jpg",
+  },
+  {
+    id: 15,
+    image: "/portfolio/Aredo Stones/Aredo Stones-06.jpg",
+  },
+  {
+    id: 16,
+    image: "/portfolio/autorental.png",
+  },
+  {
+    id: 17,
+    image: "/portfolio/autorental1.png",
+  },
+  {
+    id: 18,
+    image: "/portfolio/autorental2.png",
+  },
+  {
+    id: 19,
+    image: "/portfolio/autorental3.png",
+  },
+  {
+    id: 20,
+    image: "/portfolio/flex/2.jpg",
+  },
+  {
+    id: 22,
+    image: "/portfolio/flex/3.jpg",
+  },
+  {
+    id: 23,
+    image: "/portfolio/flex/1.jpg",
+  },
+  {
+    id: 24,
+    image: "/portfolio/flex/6.jpg",
+  },
+  {
+    id: 25,
+    image: "/portfolio/flex/7.jpg",
+  },
+  {
+    id: 26,
+    image: "/portfolio/flex/8.jpg",
+  },
+  {
+    id: 27,
+    image: "/portfolio/flex/9.jpg",
+  },
+  {
+    id: 28,
+    image: "/portfolio/flex/10.jpg",
+  },
+  {
+    id: 29,
+    image: "/portfolio/shargan1.png",
+  },
+  {
+    id: 30,
+    image: "/portfolio/shargan2.png",
+  },
+  {
+    id: 31,
+    image: "/portfolio/shargan3.png",
+  },
+  {
+    id: 32,
+    image: "/portfolio/sharganfront.png",
+  },
+  {
+    id: 33,
+    image: "/portfolio/sharganserver.png",
+  },
+  {
+    id: 34,
+    image: "/portfolio/sharganserver2.png",
+  },
+];
 
 export const designs = [
-   {
-      'id': 1,
-      'image': '/portfolio/Go Health/Go Health Brand Identity-01.png'
-   },
-   {
-      'id': 2,
-      'image': '/portfolio/Go Health/Go Health Brand Identity-02.png'
-   },
+  {
+    id: 1,
+    image: "/portfolio/Go Health/Go Health Brand Identity-01.png",
+  },
+  {
+    id: 2,
+    image: "/portfolio/Go Health/Go Health Brand Identity-02.png",
+  },
 
-   {
-      'id': 4,
-      'image': '/portfolio/Go Health/Go Health Brand Identity-03.png'
-   },
-   {
-      'id': 5,
-      'image': '/portfolio/Go Health/Go Health Brand Identity-04.png'
-   },
-   {
-      'id': 6,
-      'image': '/portfolio/Go Health/Go Health Brand Identity-05.png'
-   },
+  {
+    id: 4,
+    image: "/portfolio/Go Health/Go Health Brand Identity-03.png",
+  },
+  {
+    id: 5,
+    image: "/portfolio/Go Health/Go Health Brand Identity-04.png",
+  },
+  {
+    id: 6,
+    image: "/portfolio/Go Health/Go Health Brand Identity-05.png",
+  },
 
-   {
-      'id': 7,
-      'image': '/portfolio/Go Health/Go Health Brand Identity-06.png'
-   },
-   {
-      'id': 8,
-      'image': '/portfolio/Go Health/Go Health Brand Identity-07.png'
-   },
-   {
-      'id': 10,
-      'image': '/portfolio/Aredo Stones/Aredo Stones-01.jpg'
-   },
-   {
-      'id': 11,
-      'image': '/portfolio/Aredo Stones/Aredo Stones-02.jpg'
-   },
-   {
-      'id': 12,
-      'image': '/portfolio/Aredo Stones/Aredo Stones-03.jpg'
-   },
-   {
-      'id': 13,
-      'image': '/portfolio/Aredo Stones/Aredo Stones-04.jpg'
-   },
-   {
-      'id': 14,
-      'image': '/portfolio/Aredo Stones/Aredo Stones-05.jpg'
-   },
-   {
-      'id': 15,
-      'image': '/portfolio/Aredo Stones/Aredo Stones-06.jpg'
-   },
-
-]
+  {
+    id: 7,
+    image: "/portfolio/Go Health/Go Health Brand Identity-06.png",
+  },
+  {
+    id: 8,
+    image: "/portfolio/Go Health/Go Health Brand Identity-07.png",
+  },
+  {
+    id: 10,
+    image: "/portfolio/Aredo Stones/Aredo Stones-01.jpg",
+  },
+  {
+    id: 11,
+    image: "/portfolio/Aredo Stones/Aredo Stones-02.jpg",
+  },
+  {
+    id: 12,
+    image: "/portfolio/Aredo Stones/Aredo Stones-03.jpg",
+  },
+  {
+    id: 13,
+    image: "/portfolio/Aredo Stones/Aredo Stones-04.jpg",
+  },
+  {
+    id: 14,
+    image: "/portfolio/Aredo Stones/Aredo Stones-05.jpg",
+  },
+  {
+    id: 15,
+    image: "/portfolio/Aredo Stones/Aredo Stones-06.jpg",
+  },
+];
 
 export const switches = [
-   {
-      "id": 0,
-      'name': "All"
-   },
-   {
-      "id": 1,
-      "name": "Graphic Design"
-   },
-   {
-      "id": 2,
-      "name": "Web Development"
-   },
-   {
-      'id': 3,
-      'name': 'Social Media Management'
-   },
-   {
-      "id": 4,
-      "name": "Training"
-   }
-]
+  {
+    id: 0,
+    name: "All",
+  },
+  {
+    id: 1,
+    name: "Graphic Design",
+  },
+  {
+    id: 2,
+    name: "Web Development",
+  },
+  {
+    id: 3,
+    name: "Social Media Management",
+  },
+  {
+    id: 4,
+    name: "Training",
+  },
+];
 
 export const web = [
-   {
-      'id': 1,
-      'image': '../img/dum12.jpeg'
-   },
-   {
-      'id': 2,
-      'image': '../img/dum13.jpeg'
-   },
-   {
-      'id': 3,
-      'image': '../img/dum14.png'
-   },
-   {
-      'id': 4,
-      'image': '../img/dum15.png'
-   },
-   {
-      'id': 5,
-      'image': '../img/dum16.jpg'
-   }
-]
+  {
+    id: 1,
+    image: "../img/dum12.jpeg",
+  },
+  {
+    id: 2,
+    image: "../img/dum13.jpeg",
+  },
+  {
+    id: 3,
+    image: "../img/dum14.png",
+  },
+  {
+    id: 4,
+    image: "../img/dum15.png",
+  },
+  {
+    id: 5,
+    image: "../img/dum16.jpg",
+  },
+];
 
 export const socials = [
-   {
-      'id': 1,
-      'image': '../img/dum10.jpg'
-   },
-   {
-      'id': 2,
-      'image': '../img/dum11.jpg'
-   },
-   {
-      'id': 3,
-      'image': '../img/dum24.jpg'
-   }
-]
+  {
+    id: 1,
+    image: "../img/dum10.jpg",
+  },
+  {
+    id: 2,
+    image: "../img/dum11.jpg",
+  },
+  {
+    id: 3,
+    image: "../img/dum24.jpg",
+  },
+];
